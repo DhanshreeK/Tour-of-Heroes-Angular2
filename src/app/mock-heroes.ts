cf31b1a588c3c0aa0c2f268de172e4c39e9b6602
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-01-23 17:29:25 +0530

    added routes and services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Rohan' },
+  { id: 12, name: 'Bhushan' },
+  { id: 13, name: 'Dhanshree' },
+  { id: 14, name: 'Akash' },
+  { id: 15, name: 'Priyanka' },
+  { id: 16, name: 'Shalaka' },
+  { id: 17, name: 'Dynama' }
+];
